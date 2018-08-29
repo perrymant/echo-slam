1742860db719bc0eb5499e27b07cedb7e212551a
Thomas Perryman <tcw.perryman@gmail.com>
2018-08-29 19:25:12 +0100

    made id elements

---
+++ test-trigger.js
@@ -0,0 +1,8 @@
+var b4 = document.getElementById("b4");
+
+// function clickB4() {
+//     document.getElementById("b4").click();
+// }
+
+setInterval(function(){$(b4).toggleClass("b4activate");}, 1000);
+
